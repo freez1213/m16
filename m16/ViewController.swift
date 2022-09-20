38612e172146355ff4d4e87665b9b0b20363f7ac
Артем <freez1213@mail.ru>
2022-09-20 11:49:48 +0500

    start project. v0.1

---
+++ m16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Артем Карпенко on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
